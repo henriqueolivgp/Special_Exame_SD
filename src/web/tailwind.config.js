c6dcf87bd0c7060688a7860fc7bdf6155cea4439
henriqueolivgp <henrique.o@ipvc.pt>
2025-06-16 12:41:19 +0100

    add some breakpoints in tailwind.config.js

--- src/web/tailwind.config.js
+++ src/web/tailwind.config.js
@@ -2,10 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xxs': '250px',
+      // => @media (min-width: 250px) { ... }
+      'xs': '300px',
+      // => @media (min-width: 300px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [
     require('flowbite/plugin')
-]
+  ]
 }
 
