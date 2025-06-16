6b19df8a67cb89f19cf580ba2f61a1cd83963973
henriqueolivgp <henrique.o@ipvc.pt>
2025-06-16 13:12:47 +0100

    Created one component for protect the routes, when user isnt logged in

---
+++ src/web/src/routes/privateRoutes.js
@@ -0,0 +1,14 @@
+import { useAuth } from '../hooks/AuthHook'
+import { Navigate } from 'react-router-dom'
+
+export function PrivateRoutes ({ element, notHaveAccessNavigateTo }) {
+  const { signed, loading } = useAuth()
+
+  // console.log("loading no private routes: " + loading) // true
+
+  if(loading) {
+    return <p>Loading Page.....</p>
+  }
+
+  return signed ? element : <Navigate to={notHaveAccessNavigateTo} replace />
+}
