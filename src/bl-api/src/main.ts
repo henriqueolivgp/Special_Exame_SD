9c8fd21ee45c8a0447816e52ac15c784b2800533
henriqueolivgp <henrique.o@ipvc.pt>
2025-06-17 21:35:30 +0100

    delete swaggerApp and Swagger server and modify cors config

--- src/bl-api/src/main.ts
+++ src/bl-api/src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const port = process.env.BL_API_PORT || 8080;
-  const portSwagger = process.env.SWAGGER_PORT;
+  // const portSwagger = process.env.SWAGGER_PORT;
 
   const config = new DocumentBuilder()
     .setTitle('Movies')
@@ -17,27 +17,29 @@ async function bootstrap() {
     .build();
 
   try {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule); // , { cors: true }
+
     app.enableCors({
-      origin: '*', // Substitua pelo domínio da sua API Express
+      origin: 'http://localhost:70',
       credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // opcional, mas melhor que só GET
     });
+
     app.use(cookieParser());
+
+    // Swagger
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/docs', app, document);
+
+    app.use((req, res, next) => {
+      console.log('Origin:', req.headers.origin);
+      next();
+    });
 
     await app.listen(port, () => {
       const address = `http://localhost:${port}`;
       logger.log(`Server is running on ${address}`);
-    });
-
-    // Swagger
-    const swaggerApp = await NestFactory.create(AppModule);
-    const document = SwaggerModule.createDocument(swaggerApp, config);
-    SwaggerModule.setup('/docs', swaggerApp, document);
-
-    // Start the Swagger app
-    await swaggerApp.listen(portSwagger, () => {
-      const swaggerAddress = `http://localhost:${portSwagger}`;
-      logger.log(`Swagger is running on ${swaggerAddress}/docs`);
+      logger.log(`Swagger is running on ${address}/docs`);
     });
   } catch (err) {
     logger.error('Error starting server:', err);
