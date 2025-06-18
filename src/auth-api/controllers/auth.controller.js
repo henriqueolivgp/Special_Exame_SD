d4c9e3aa31e35e716433513b555f5983b0e956bb
henriqueolivgp <henrique.o@ipvc.pt>
2025-06-18 20:03:12 +0100

    Remove comments and organize the code

--- src/auth-api/controllers/auth.controller.js
+++ src/auth-api/controllers/auth.controller.js
@@ -3,9 +3,9 @@ const authService = require('../services/auth.service')
 const verifyRegister = async (req, res) => {
   try {
     const { username, password, role = 'admin' } = req.body;
-    console.log(username, password, role)
 
     await authService.register(username, password, role)
+
     res.status(200).json({ message: 'User make a sucefull sign-up' })
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -16,10 +16,11 @@ const verifyLogin = async (req, res) => {
   try {
     // console.log('entrei no controller')
     const { username, password } = req.body;
+
     const { token, user } = await authService.login(username, password);
+
     // console.log('antes dos cookies')
     res.cookie('token', token, { httpOnly: true });
-    // console.log('depois dos cookies')
 
     res.status(200).json({ message: 'User successfully logged in', token, user });
   } catch (error) {
--- src/auth-api/services/auth.service.js
+++ src/auth-api/services/auth.service.js
@@ -7,9 +7,7 @@ const validRoles = ['admin', 'view', 'edit'];
 
 const register = async (username, password, role = 'admin') => {
   try {
-    console.log('A comecar registro de utilizador'); // Log de início
     // Verifica se o usuário já existe
-    console.log('Verificando se o usuário já existe');
     const existingUser = await knex('users').where({ username }).first();
     if (existingUser) {
       throw new Error('Este username já está em uso.');
@@ -19,18 +17,15 @@ const register = async (username, password, role = 'admin') => {
       throw new Error('Role inválido. Os roles permitidos são: admin, view, edit.');
     }
     // Cria o hash da senha em sha256
-    console.log('A criar a hash da senha fornecida');
     const passwordHashed = crypto.createHash('sha256').update(password).digest('hex');
 
     // Insere o novo usuário no banco de dados
-    console.log('A inserir utilizador na bd');
     await knex('users').insert({
       username,
       password: passwordHashed,
       role: role,
     });
 
-    console.log('Usuário registrado com sucesso'); // Log de sucesso
     return { message: 'User registered successfully' };
   } catch (error) {
     throw new Error('Error in sign-up user: ' + error.message);
@@ -42,21 +37,13 @@ const login = async (username, password) => {
     // console.log('Iniciando login de usuário'); 
 
     const user = await knex('users').where({ username }).first();
-    if (!user) {
-         Error('Invalid username, this username dont exists');
-    }
 
     // Hash da senha fornecida e comparação com a senha armazenada
-    console.log('Criando hash da senha para comparação');
     const passwordHashed = crypto.createHash('sha256').update(password).digest('hex');
 
-    if (user.password !== passwordHashed) {
-      throw new Error('Invalid password. Pls try again or resete you password');
+    if (user.password !== passwordHashed && !user) {
+      throw new Error('Invalid credentials. Pls check the inputs!!');
     }
-
-    console.log('Usuário autenticado com sucesso'); // Log de sucesso
-
-    console.log(user.role)
 
     // craicao de um objeto que vai conter algun dados importantes do utilizador
     const tokenPayload = {
@@ -64,7 +51,6 @@ const login = async (username, password) => {
     }
 
     const token = jwt.sign({ tokenPayload }, 'sd', { expiresIn: '1d', subject: user.id.toString() });
-    console.log(token);
     
     return { token, user };
   } catch (error) {
