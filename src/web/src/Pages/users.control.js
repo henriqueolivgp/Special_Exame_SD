b6f976aa9d256bd432fe5ff5105de0c879e35570
henriqueolivgp <henrique.o@ipvc.pt>
2025-06-18 17:14:02 +0100

    user control page for admins

---
+++ src/web/src/Pages/users.control.js
@@ -0,0 +1,21 @@
+import {
+  Container,
+} from "@mui/material";
+
+export default function UsersControl() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-10 text-white">Users Control</h1>
+      <Container
+        maxWidth="100%"
+        sx={{
+          backgroundColor: "background.default",
+          padding: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+
+      </Container>
+    </>
+  )
+}
