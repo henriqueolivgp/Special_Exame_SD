da6a8746712700b540e474d46085159ad327f53c
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2026-07-17 11:45:55 +0100

    fix: try 1

--- src/web/src/providers/AuthProvider.js
+++ src/web/src/providers/AuthProvider.js
@@ -59,7 +59,7 @@ export const AuthProvider = ({ children }) => {
       console.log('dados antes da requesicao:' + username + ' - ' + password)
 
       // make the request into my auth api for logged in
-      const response = await axios.post('http://localhost:18080/login', {
+      const response = await axios.post(`${process.env.REACT_APP_AUTH_API_URL}/login`, {
         username,
         password
       });
