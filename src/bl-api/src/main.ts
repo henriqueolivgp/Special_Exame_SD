014206dcacd990997198fe4fdbf7b8492b8ae9ff
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2026-07-17 10:03:10 +0100

    fix:cors errors

--- src/bl-api/src/main.ts
+++ src/bl-api/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule); // , { cors: true }
 
     app.enableCors({
-      origin: 'http://localhost:70',
+      origin: 'http://b72r7mi6l7yrh23zskd2o8ok.51.170.48.125.sslip.io',
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE'], // opcional, mas melhor que só GET
     });
