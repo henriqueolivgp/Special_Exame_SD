f2212a9cf1ae077a7618adb91dd588973f973e83
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2026-07-20 16:37:54 +0100

    refactor: change colors in tailwind.cofig

--- src/web/tailwind.config.js
+++ src/web/tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      chalk: '#F4F4F5', // Coloca aqui o código da tua cor clara
-      ink: '#18181B',   // Coloca aqui o código da tua cor escura
+      chalk: '#18181B', // Coloca aqui o código da tua cor clara
+      ink: '#F4F4F5',   // Coloca aqui o código da tua cor escura
     },
     screens: {
       'xxs': '250px',
